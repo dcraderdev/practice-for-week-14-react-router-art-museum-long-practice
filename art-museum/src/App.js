d8ecc7d8d618d05e97572a5fd1ddbb3b7397b0d2
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-03-02 15:34:40 -0800

    phase 0 complete

---
+++ art-museum/src/App.js
@@ -0,0 +1,13 @@
+import harvardArt from './data/harvardArt.js'
+
+function App() {
+  console.log('-=-=-=-=-=-');
+  console.log(harvardArt);
+  console.log('-=-=-=-=-=-');
+
+  return (
+    <h1>Hello from App</h1>
+  );
+}
+
+export default App;
